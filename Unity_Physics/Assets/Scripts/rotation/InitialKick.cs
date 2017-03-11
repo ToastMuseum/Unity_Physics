0e4ae858579195bc8cc3d01dee2a290562774bc5
toastmuseum <Deolijus@gmail.com>
2017-03-11 18:34:08 -0500

    Introduce rotation
    Add a script to perform default rotation on a rigidbody. Check
    performance of basic rotation vs corrected rotation script using the
    Window->profiler  tool.

---
+++ Unity_Physics/Assets/Scripts/rotation/InitialKick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class InitialKick : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+
+	void OnEnable(){
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.angularVelocity = new Vector3 (4f, 0, 0);
+	}
+
+}
