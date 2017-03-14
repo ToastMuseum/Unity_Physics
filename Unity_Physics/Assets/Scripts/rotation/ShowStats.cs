d5e4e59a77669dbc9deda753a1535ed8620dd938
toastmuseum <Deolijus@gmail.com>
2017-03-13 23:13:06 -0400

    Introduce Intertia Tensors
    ShowStats script
    using [ExecuteInEditMode] displays content even when out of play mode.
    The axis by which an object will not rotate evenly is about the
    intermediate axis or the axis (in between largest and smallest value)

---
+++ Unity_Physics/Assets/Scripts/rotation/ShowStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class ShowStats : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(rigidBody.inertiaTensor);
+	}
+}
