ba599c0db66b0cbaacd390dc630d220f6a1e4953
toastmuseum <Deolijus@gmail.com>
2017-03-15 13:55:16 -0400

    Magnus Effect Script
    Demonstrate the magnus effect on a sphere based on the magnus force
    produced by linear and angular velocity of the object
    
    F = magnusConstant * (v cross w)

---
+++ Unity_Physics/Assets/Scripts/Magnus Effect/InitialVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialVelocity : MonoBehaviour {
+
+	public Vector3 initialVelocity;
+	public Vector3 initialAngularVelocity;
+
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.velocity = initialVelocity;
+		rigidBody.angularVelocity = initialAngularVelocity;
+	}
+
+}
---
+++ Unity_Physics/Assets/Scripts/Magnus Effect/MagnusEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour {
+
+	public float magnusConstant = 1f;
+
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidBody.AddForce (magnusConstant * Vector3.Cross (rigidBody.angularVelocity, rigidBody.velocity) * Time.deltaTime);
+	}
+}
